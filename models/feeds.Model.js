948ed1f0bd6e385ef968640caae8ed2f83f3aa47
Dominic-SR <dominicseduraja3@gmail.com>
2022-04-19 16:52:03 +0530

    feed question get by interest fixed

--- models/feeds.Model.js
+++ models/feeds.Model.js
@@ -17,14 +17,14 @@ const FeedsModel = {
 
         if(interests){
         
-        let query = `select * from feeds order by created_at DESC`;
+        let query = `select f.feed_id,f.feed_summary,f.feed_image,f.interests,f.user_id,f.created_at,f.updated_at,u.user_id,u.full_name,u.designation,u.work_experience,u.email,u.mobile_no,u.profile_picture,u.fcm_token ,(SELECT click_user_id FROM feed_likes c where c.feed_id = f.feed_id AND c.click_user_id=${user_id}) as liked_user,(select count(*) from feed_likes c where c.feed_id = f.feed_id) as feed_likes,(select count(*) from feed_comments m where m.feed_id = f.feed_id) as feed_comments from feeds f inner join users u on u.user_id = f.user_id order by f.created_at DESC`;
         //let query = `select f.feed_id,f.feed_summary,f.feed_image,f.interests,f.user_id,f.created_at,f.updated_at,(select count(*) from feed_likes c where c.feed_id = f.feed_id) as feed_likes, (select count (*) from feed_comments m where m.feed_id = f.feed_id) as feed_comments from feeds f where interests like "%${data.interests}%" order by created_at DESC`;
 
         // let query = `select * ,(select IF(user_id IS NULL, FALSE, TRUE)as liked from feed_likes c where c.click_user_id=${user_id}) (select count(*) from feed_likes c where c.feed_id = f.feed_id) as feed_likes,(select count(*) from feed_comments m where m.feed_id = f.feed_id) as feed_comments from feeds f inner join users u on u.user_id = f.user_id where f.interests like "%${interests}%"`
         // let query = `select f.feed_id,f.feed_summary,f.feed_image,f.interests,f.user_id,f.created_at,f.updated_at,u.user_id,u.full_name,u.designation,u.work_experience,u.email,u.mobile_no,u.profile_picture,u.fcm_token ,(SELECT click_user_id FROM feed_likes c where c.feed_id = f.feed_id AND c.click_user_id=${user_id}) as liked_user,(select count(*) from feed_likes c where c.feed_id = f.feed_id) as feed_likes,(select count(*) from feed_comments m where m.feed_id = f.feed_id) 
         // as feed_comments from feeds f inner join users u on u.user_id = f.user_id where f.interests like "[%${interests}%]" or f.interests like "%${interests}" order by f.created_at DESC
         // `
-
+        console.log("query---------------------->",query)
         return database.promise().query(query)
         }
     },
--- models/question.Model.js
+++ models/question.Model.js
@@ -22,7 +22,7 @@ const QuestionModel = {
         }
 
         if(interests){
-        let query = `select * from questions order by created_at DESC`
+        let query = `select q.question_id,q.question_summary,q.question_image,q.interests,q.user_id,q.question_views,q.created_at,q.updated_at,u.full_name,u.designation,u.work_experience,u.email,u.mobile_no,u.profile_picture,u.fcm_token ,(select count (*) from questions_likes l where l.questions_id = q.question_id) as question_likes, (select count (*) from questions_comments l where l.questions_id = q.question_id) as question_comments,(SELECT click_user_id FROM questions_likes c where c.questions_id = q.question_id AND c.click_user_id=${user_id}) as liked_user from questions q inner join users u on u.user_id = q.user_id order by created_at DESC`
             //let query = `select q.question_id,q.question_summary,q.question_image,q.interests,q.user_id,q.question_views,q.created_at,q.updated_at,u.full_name,u.designation,u.work_experience,u.email,u.mobile_no,u.profile_picture,u.fcm_token ,(select count (*) from questions_likes l where l.questions_id = q.question_id) as question_likes, (select count (*) from questions_comments l where l.questions_id = q.question_id) as question_comments,(SELECT click_user_id FROM questions_likes c where c.questions_id = q.question_id AND c.click_user_id=${user_id}) as liked_user from questions q inner join users u on u.user_id = q.user_id where q.interests like "%${interests}%" order by created_at DESC`;
         return database.promise().query(query)
         }
